e8395a29267e9869442ae4e04bc0a904bbf05ec1
Dan Mopsick <danmopsick@gmail.com>
2022-10-09 17:20:19 -0400

    #14 #15 rerranging the files. Checking in the new names. Now to test

---
+++ database/sql/ddl/03_createUserTable.sql
@@ -0,0 +1,28 @@
+-----------------------------------------
+---- DDL Statements for the USER Table
+-----------------------------------------
+
+CREATE TABLE USER (
+    USER_ID             INT NOT NULL AUTO_INCREMENT,
+    USER_SETTINGS_ID    INT NOT NULL,
+    USER_ROLE_ID        INT NOT NULL,
+    USER_EMAIL          VARCHAR(128) NOT NULL
+    USER_FIRST_NAME     VARCHAR(128) NOT NULL,
+    USER_LAST_NAME      VARCHAR(128) NOT NULL,
+    USER_PREFERRED_NAME VARCHAR(128),
+    USER_ACTIVE         BOOLEAN
+);
+
+-- DDL Statements for indexes on USER Table
+CREATE UNIQUE INDEX USER_UQ1 
+ON USER ("USER_ID");
+
+CREATE INDEX USER_IDX2
+ON USER ("USER_SETTINGS_ID");
+
+ALTER TABLE USER
+ADD PRIMARY KEY ("USER_ID");
+
+-- DDL Statement for Foreign Keys on USER Table
+ALTER TABLE USER
+ADD FOREIGN KEY ("USER_SETTINGS_ID") REFERENCES USER_SETTINGS("USER_SETTINGS_ID");
---
+++ database/sql/ddl/04_createTrainerTable.sql
@@ -0,0 +1,25 @@
+-----------------------------------------
+---- DDL Statements for the TRAINER Table
+-----------------------------------------
+
+CREATE TABLE TRAINER (
+    TRAINER_ID            INT NOT NULL AUTO_INCREMENT,
+    USER_ID               INT NOT NULL,
+    TRAINER_WEBSITE_URL   VARCHAR(256),
+    TRAINER_YOUTUBE_URL   VARCHAR(256),
+    TRAINER_INSTAGRAM_URL VARCHAR(256)
+);
+
+-- DDL Statements for indexes on TRAINER Table
+CREATE UNIQUE INDEX TRAINER_UNQ1 
+ON TRAINER ("TRAINER_ID");
+
+CREATE INDEX TRAINER_IDX2
+ON TRAINER ("USER_ID");
+
+ALTER TABLE TRAINER
+ADD PRIMARY KEY ("TRAINER_ID");
+
+-- DDL Statement for Foreign Keys on TRAINER Table
+ALTER TABLE TRAINER
+ADD FOREIGN KEY ("USER_ID") REFERENCES USER("USER_ID");
