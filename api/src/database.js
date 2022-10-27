45b645174f44fcb5067ab39b75cb0d53b7fae3ca
Dan Mopsick <danmopsick@gmail.com>
2022-10-27 19:22:43 -0400

    #16 moved the credentials to a .env much more secure

--- api/src/database.js
+++ api/src/database.js
@@ -1,11 +1,21 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+// Load the MySQL creds from .env
+dotenv.config();
+
+const host = process.env.MYSQL_HOST;
+const port = process.env.MYSQL_PORT;
+const user = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const db = process.env.MYSQL_DATABASE;
 
 const pool = mysql.createPool({
-    host: 'cantrain-db-2.cfnemm1rnekh.us-east-1.rds.amazonaws.com',
-    port: '3306',
-    user: 'admin',
-    password: '', // DO NOT SAVE THIS IN VERSION CONTROL
-    database: 'CANTRAINDB'
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    database: db
 }).promise();
 
 const result = await pool.query("SELECT * FROM USER");
