6b6b3ebc9116a41698a58beaf08aff9902bdc2f5
Dan Mopsick <danmopsick@gmail.com>
2022-11-12 14:46:48 -0500

    #28 abstracting the actual content of the hoem page into its own file so I can get the actual bottom nav logic going

--- front-end/cantrain/lib/views/home_page.dart
+++ front-end/cantrain/lib/views/home_page.dart
@@ -1,7 +1,6 @@
+import 'package:cantrain/views/home_dashboard_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cantrain/models/dbuser.dart';
-import 'package:cantrain/services/api_service.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeView extends StatefulWidget {
@@ -14,90 +13,66 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   // The authenticated user object from FireBase
   final authUser = FirebaseAuth.instance.currentUser!;
-  // The current user's info from the DB
-  DBUser? currentUser;
   var isLoaded = false;
+  int selectedBottomNavIndex = 0;
+  final List<Widget> pages = [
+    const HomeDashboardView(),
+    // RecordsView(),
+    // AccountView()
+  ];
 
   @override
   void initState() {
     super.initState();
-
-    // Load the current user's info from the DB
-    loadUser(authUser.email);
   }
 
-  // I will need to call this function from every file
-  // Will need to house it somewhere else and import it or something
-  loadUser(email) async {
-    // Load the user from the API
-    currentUser = await ApiService().getUserByEmail(email);
-
-    // If the user is loaded, load the UI
-    if (currentUser != null) {
+    void navigate(int index) {
       setState(() {
-        isLoaded = true;
+        selectedBottomNavIndex = index;
       });
-    }
 
-  }
+    }
 
   @override
   Widget build(BuildContext context) {
     
-    return Visibility(
-        // Show the UI after loading what we need from the DB
-        visible: isLoaded,
-        child: Scaffold (
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Signed in as ${currentUser?.email}. Hello ${currentUser?.firstName} ${currentUser?.lastName}'),
-                MaterialButton (
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  },
-                  color: Colors.indigo[700],
-                  child: const Text(
-                    'Sign Out',
-                    style : TextStyle (color: Colors.white),
-                  ),
-                )
-              ],
-            ),
-            ),
-            
-            bottomNavigationBar: Container(
-              color: Colors.blue.shade900,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
-                child: GNav(
-                  gap: 8,
-                  backgroundColor: Colors.blue.shade900,
-                  color: Colors.white,
-                  activeColor: Colors.white,
-                  tabBackgroundColor: Colors.blue.shade800,
-                  padding: EdgeInsets.all(16),
-                  tabs: const [
-                    GButton(
-                      icon: Icons.home,
-                      text: 'Home'
-                    ),
-                    GButton(
-                      icon: Icons.library_books,
-                      text: 'Records'
-                    ),
-                    GButton(
-                      icon: Icons.account_circle,
-                      text: 'Account'
-                    ),   
-                  ]
-                ),
+    return Scaffold(
+      body: pages[selectedBottomNavIndex],
+      bottomNavigationBar: Container(
+        color: Colors.blue.shade900,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+          child: GNav(
+            gap: 8,
+            onTabChange: (index) {
+              print(index);
+            },
+            backgroundColor: Colors.blue.shade900,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.blue.shade800,
+            padding: EdgeInsets.all(16),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home'
               ),
-            ),
-
+              GButton(
+                icon: Icons.library_books,
+                text: 'Records'
+              ),
+              GButton(
+                icon: Icons.account_circle,
+                text: 'Account'
+              ),   
+            ]
+          ),
         ),
-      );
+      ),
+
+  );
+      
   
   }
 }
+
