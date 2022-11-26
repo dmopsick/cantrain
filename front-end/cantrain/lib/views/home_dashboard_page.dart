448b7e5792aa66058db29b78242ce5d1a5564dc4
Dan Mopsick <danmopsick@gmail.com>
2022-11-26 17:32:14 -0500

    #31 I have a list of assigned regiments... now need to just be able to click them and open up detail view

--- front-end/cantrain/lib/views/home_dashboard_page.dart
+++ front-end/cantrain/lib/views/home_dashboard_page.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cantrain/models/dbuser.dart';
 import 'package:cantrain/services/api_service.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeDashboardView extends StatefulWidget {
   const HomeDashboardView({super.key});
@@ -31,6 +31,11 @@ class _HomeDashboardViewState extends State<HomeDashboardView> {
     loadFromDB(authUser.email);
   }
 
+  // Function to open up selected Assigned Regiment
+  void openAssignedRegiment(BuildContext context) {
+    print("Flag 10");
+  }
+
   // I will need to call this function from every file
   // Will need to house it somewhere else and import it or something
   loadFromDB(email) async {
@@ -39,11 +44,8 @@ class _HomeDashboardViewState extends State<HomeDashboardView> {
 
     // If the user is loaded, load the UI
     if (currentUser != null) {
-      // Load the assigned regiments for the currentUser 
+      // Load the assigned regiments for the currentUser
       assignedRegimentList = await AssignedRegimentApiService().getAssignedRegimentListByUser(currentUser);
-
-      print('Flag 1 ${assignedRegimentList![0].id}');
-      print('Flag 2 ${assignedRegimentList![0].regiment.name}');
 
       setState(() {
         isLoaded = true;
@@ -53,32 +55,72 @@ class _HomeDashboardViewState extends State<HomeDashboardView> {
 
   @override
   Widget build(BuildContext context) {
-    
-    return Visibility(
-        // Show the UI after loading what we need from the DB
+      return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Assigned Regiments',
+           style: GoogleFonts.bebasNeue(),
+          ),
+      ),
+      body: Visibility(
         visible: isLoaded,
-        child: Scaffold (
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 100),
-                Center(child: Text('Hello ${currentUser?.firstName}')),
-                // ListView.builder(
-                //   itemCount: assignedRegimentList?.length,
-                //   itemBuilder: (context, index) {
-                //     return Center(
-                //       child: Text('Hello')
-                //     );
-                //   }
-                // )
-              ],
-            ),
-            ),
-      
+        replacement: const Center(
+          child: CircularProgressIndicator(),
         ),
-      );
-    
-    }
-
+        child: ListView.builder(
+          itemCount: assignedRegimentList?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[300],
+                    ),
+                    child: const Icon(
+                        Icons.fitness_center,
+                        size: 50
+                      ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Flag 2 ${assignedRegimentList![index].id}");
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            assignedRegimentList![index].regiment.name,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            assignedRegimentList![index].regiment.name ?? '',
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.bebasNeue(),
+                          ),
+                                      
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
+}
